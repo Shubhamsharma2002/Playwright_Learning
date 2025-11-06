a703ebab7dcfb51a79b2139a2c405a2f85abe582
shubham sharma <shubhamtech004@gmail.com>
2025-11-06 23:27:53 +0530

    first commit

---
+++ tests/HomePageTest.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Home Page', async ({ page }) => {
+  // Navigate to site
+  await page.goto('https://www.demoblaze.com/index.html');
+
+  // Get page title (with await)
+  const pageTitle = await page.title();
+  console.log("Page title:", pageTitle);
+
+  // Assertions
+  await expect(page).toHaveTitle('STORE');
+  await expect(page).toHaveURL('https://www.demoblaze.com/index.html');
+});
