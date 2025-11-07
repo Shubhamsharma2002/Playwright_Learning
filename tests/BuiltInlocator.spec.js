a47d83590e7d5a280f0f360e9afddd3f8bf1e2ba
shubham sharma <shubhamtech004@gmail.com>
2025-11-07 18:29:18 +0530

    some bult-in locators

---
+++ tests/BuiltInlocator.spec.js
@@ -0,0 +1,18 @@
+import {test,expect} from "@playwright/test";
+
+test("Built-In-locator", async({page})=>{
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    // logo alt text
+    const logo = await page.getByAltText("company-branding");
+    await expect(logo).toBeVisible(); 
+
+    // username ,password placeholder
+     await page.getByPlaceholder("Username").fill("Admin");
+     await page.getByPlaceholder("password").fill("admin123");
+    
+    await page.getByRole('button', {type:'submit'}).click();
+    
+    const name = await page.locator("//p[@class='oxd-userdropdown-name']").textContent();
+    await expect(await page.getByText(name)).toBeVisible();
+})//p[@class='oxd-userdropdown-name']
