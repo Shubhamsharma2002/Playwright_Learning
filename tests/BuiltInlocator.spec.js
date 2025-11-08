08d35cdc69eb30e7ec1b9f92a7cd827ba784fe3b
shubham sharma <shubhamtech004@gmail.com>
2025-11-08 23:02:26 +0530

    some bult-in locators

--- tests/BuiltInlocator.spec.js
+++ tests/BuiltInlocator.spec.js
@@ -1,5 +1,6 @@
 import {test,expect} from "@playwright/test";
 
+
 test("Built-In-locator", async({page})=>{
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
@@ -8,7 +9,7 @@ test("Built-In-locator", async({page})=>{
     await expect(logo).toBeVisible(); 
 
 
-    
+
     // username ,password placeholder
      await page.getByPlaceholder("Username").fill("Admin");
      await page.getByPlaceholder("password").fill("admin123");
