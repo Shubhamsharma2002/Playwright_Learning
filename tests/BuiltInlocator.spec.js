bc6333d224539bd49265539607a45dd3bf6ce417
shubham sharma <shubhamtech004@gmail.com>
2025-11-08 23:02:50 +0530

    some bult-in locators

--- tests/BuiltInlocator.spec.js
+++ tests/BuiltInlocator.spec.js
@@ -1,22 +1,22 @@
-import {test,expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
+test("Built-In-locator", async ({ page }) => {
+  await page.goto(
+    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+  );
+  // logo alt text
+  const logo = await page.getByAltText("company-branding");
+  await expect(logo).toBeVisible();
 
-test("Built-In-locator", async({page})=>{
+  // username ,password placeholder
+  await page.getByPlaceholder("Username").fill("Admin");
+  await page.getByPlaceholder("password").fill("admin123");
 
-    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    // logo alt text
-    const logo = await page.getByAltText("company-branding");
-    await expect(logo).toBeVisible(); 
+  //  button test cases
+  await page.getByRole("button", { type: "submit" }).click();
 
-
-
-    // username ,password placeholder
-     await page.getByPlaceholder("Username").fill("Admin");
-     await page.getByPlaceholder("password").fill("admin123");
-    
-    //  button test cases
-    await page.getByRole('button', {type:'submit'}).click();
-    
-    const name = await page.locator("//p[@class='oxd-userdropdown-name']").textContent();
-    await expect(await page.getByText(name)).toBeVisible();
-})
+  const name = await page
+    .locator("//p[@class='oxd-userdropdown-name']")
+    .textContent();
+  await expect(await page.getByText(name)).toBeVisible();
+});
