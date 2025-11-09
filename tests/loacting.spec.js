e7daeb729466beadbbdb892b9e653449375a6b72
shubham sharma <shubhamtech004@gmail.com>
2025-11-09 23:26:38 +0530

    testing

--- tests/loacting.spec.js
+++ tests/loacting.spec.js
@@ -1,16 +1,12 @@
-import {test,expect} from"@playwright/test"
+import { test, expect } from "@playwright/test";
 
-test("Locating Anchor", async({page})=>{
-
-    await page.goto('https://www.demoblaze.com/index.html')
-    //  const links = await page.$$("a");
-    //  console.log(`Total links found: ${links.length}`);
-    const links = await page.$$('a')
-    for(const li of links){
-       
-        const inerText = (await li.textContent())?.trim();
-        console.log(inerText);
-        
-        
-    }
-})
+test("Locating Anchor", async ({ page }) => {
+  await page.goto("https://www.demoblaze.com/index.html");
+  //  const links = await page.$$("a");
+  //  console.log(`Total links found: ${links.length}`);
+  const links = await page.$$("a");
+  for (const li of links) {
+    const inerText = (await li.textContent())?.trim();
+    console.log(inerText);
+  }
+});
