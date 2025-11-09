ee5e1d58f885c4fe9fc3ec1cde7ad172bc31559a
shubham sharma <shubhamtech004@gmail.com>
2025-11-09 23:27:11 +0530

    testing

--- tests/HomePageTest.spec.js
+++ tests/HomePageTest.spec.js
@@ -1,16 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-
-
-test('Home Page', async ({ page }) => {
+test("Home Page", async ({ page }) => {
   // Navigate to site
-  await page.goto('https://www.demoblaze.com/index.html');
+  await page.goto("https://www.demoblaze.com/index.html");
 
   // Get page title (with await)
   const pageTitle = await page.title();
   console.log("Page title:", pageTitle);
 
   // Assertions
-  await expect(page).toHaveTitle('STORE');
-  await expect(page).toHaveURL('https://www.demoblaze.com/index.html');
+  await expect(page).toHaveTitle("STORE");
+  await expect(page).toHaveURL("https://www.demoblaze.com/index.html");
 });
