d3810b39635b2abc8161a73b1627b162479db96a
shubham sharma <shubhamtech004@gmail.com>
2025-11-10 23:28:48 +0530

    testing

--- tests/loacting.spec.js
+++ tests/loacting.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+
+
 test("Locating Anchor", async ({ page }) => {
   await page.goto("https://www.demoblaze.com/index.html");
   //  const links = await page.$$("a");
