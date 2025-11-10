bb4a3270c830df67165375665b4943407887530f
shubham sharma <shubhamtech004@gmail.com>
2025-11-10 23:28:17 +0530

    testing

--- tests/BuiltInlocator.spec.js
+++ tests/BuiltInlocator.spec.js
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 
+
 test("Built-In-locator", async ({ page }) => {
   await page.goto(
     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
