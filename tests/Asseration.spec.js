0bcc7c2bf20a2cc7bf151934c6b9174c14cd0d29
shubham sharma <shubhamtech004@gmail.com>
2025-11-17 23:31:47 +0530

    testing

--- tests/Asseration.spec.js
+++ tests/Asseration.spec.js
@@ -1,21 +1,22 @@
-import {test,expect} from "@playwright/test"
-test("Asseration tset",async({page})=>{
-    
-    // url link 
+import { test, expect } from "@playwright/test";
+test("Asseration tset", async ({ page }) => {
+  // url link
 
-    await page.goto('https://demo.nopcommerce.com/register?returnUrl')
+  await page.goto("https://demo.nopcommerce.com/register?returnUrl");
 
-    // 1) check the url is right or not
+  // 1) check the url is right or not
 
-    await expect(page).toHaveURL('https://demo.nopcommerce.com/register?returnUrl');
+  await expect(page).toHaveURL(
+    "https://demo.nopcommerce.com/register?returnUrl"
+  );
 
-    // 2) check the page title 
+  // 2) check the page title
 
-    await expect(page).toHaveTitle('nopCommerce demo store. Register');
+  await expect(page).toHaveTitle("nopCommerce demo store. Register");
 
-    // 3) check the logo is visible or not 
+  // 3) check the logo is visible or not
 
-   const logoElement =  await page.locator('.header-logo') ;
+  const logoElement = await page.locator(".header-logo");
 
-   await expect(logoElement).toBeVisible();
-})
+  await expect(logoElement).toBeVisible();
+});
