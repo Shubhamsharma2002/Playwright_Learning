4eb816342ac38f5519da69bb22a1ce7e28ad16ba
shubham sharma <shubhamtech004@gmail.com>
2025-11-17 23:32:04 +0530

    testing

--- tests/Codegen.spec.js
+++ tests/Codegen.spec.js
@@ -1,15 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  //  page link 
-  await page.goto('https://www.demoblaze.com/index.html');
-  await page.getByRole('link', { name: 'Log in' }).click();
+test("test", async ({ page }) => {
+  //  page link
+  await page.goto("https://www.demoblaze.com/index.html");
+  await page.getByRole("link", { name: "Log in" }).click();
   //  login username input
-  await page.locator('#loginusername').click();
-  await page.locator('#loginusername').fill('pavanol');
-  // login password input 
-  await page.locator('#loginpassword').click();
-  await page.locator('#loginpassword').fill('test@123');
-  await page.getByRole('button', { name: 'Log in' }).click();
-  await page.getByRole('link', { name: 'Log out' }).click();
+  await page.locator("#loginusername").click();
+  await page.locator("#loginusername").fill("pavanol");
+  // login password input
+  await page.locator("#loginpassword").click();
+  await page.locator("#loginpassword").fill("test@123");
+  await page.getByRole("button", { name: "Log in" }).click();
+  await page.getByRole("link", { name: "Log out" }).click();
 });
